1d9dc83d18473e24a7d04ac2275344e78fcf025a
Cher Stewart <cstewart@gannett.com>
2015-07-26 16:31:38 -0700

    initial twitch stuff

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Heroes of the Storm: A Tournament Overlay' });
 });
 
 module.exports = router;
